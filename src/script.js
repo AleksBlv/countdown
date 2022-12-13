e93c933af5ad0fee22b954c72744ea5d1ab5046b
Aleksey Belyaev <aleksey.belyaev@mytona.com>
2022-12-13 21:58:44 +0100

    initial

---
+++ src/script.js
@@ -0,0 +1,41 @@
+const newYearDate = '1 Jan 2023';
+
+function format(val){
+    if(val < 10){
+        return '0' + val;
+    }
+    return val;
+}
+
+
+function updateTimer(){
+    const dday = new Date(newYearDate);
+    const today = new Date();
+
+    let deltaSeconds = Math.floor((dday - today)/1000);
+
+    const days = Math.floor(deltaSeconds / 86400)
+    deltaSeconds = deltaSeconds - days * 86400;
+
+    const hours = Math.floor(deltaSeconds / 3600);
+    deltaSeconds = deltaSeconds - hours * 3600;
+
+    const mins = Math.floor(deltaSeconds / 60);
+    deltaSeconds = deltaSeconds - mins * 60;
+
+    const seconds = deltaSeconds;
+
+    let daysHolder = document.getElementById('days');
+    daysHolder.textContent = days;
+
+    let hoursHolder = document.getElementById('hours');
+    hoursHolder.textContent = format(hours);
+
+    let minsHolder = document.getElementById('mins');
+    minsHolder.textContent = format(mins);
+
+    let secondsHolder = document.getElementById('seconds');
+    secondsHolder.textContent = format(seconds);
+}
+
+setInterval(updateTimer, 1000);
